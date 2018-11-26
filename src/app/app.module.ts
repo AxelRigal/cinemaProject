a65a9012d373970dc2f852ebb0398a48f4964352
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2018-11-26 17:08:48 +0100

    add Home web page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './home/mon-premier/mon-premier.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import { BodyComponent } from './home/body/body.component';
+import { SearchComponent } from './home/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    HomeComponent,
+    LoginComponent,
+    BodyComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
