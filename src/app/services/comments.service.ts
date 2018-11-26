b7208da572b2bd9668d49d7be5f2997a9a55aefc
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2018-11-26 23:59:52 +0100

    comments et films services

--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { Comment } from '../models/Comment.models';
+import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
+  comments: Comment[] = [];
+  commentSubject = new Subject<Comment[]>();
   constructor() { }
+
+  emitComments(){
+    this.commentSubject.next(this.comments);
+  }
+
+  saveComments(){
+    firebase.database().ref('/comments').set(this.comments);
+  }
+
+  getComments(){
+    firebase.database().ref('/comments')
+      .on('value', (data) =>{
+          this.comments = data.val() ? data.val() : [];
+          this.emitComments();
+      });
+  }
+
+  getSingleComment(id:number){
+    return new Promise(
+      (resolve, reject) =>{
+        firebase.database().ref('/comments/'+id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+      }
+    )
+  }
+  createNewComment(newComment: Comment){
+    this.comments.push(newComment);
+    this.emitComments();
+  }
+
+  removeComment(comment: Comment){
+    const commentIndexToRemove = this.comments.findIndex(
+      (comEl) =>{
+        if(comEl === comment){
+          return true;
+        }
+      }
+    );
+    this.comments.splice(commentIndexToRemove, 1);
+    this.saveComments();
+    this.emitComments();
+  }
 }
--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
-
+import { Film } from '../models/Film.models';
+import * as firebase from 'firebase';
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class FilmsService {
-
+  films: Film[] = [];
+  filmsSubject = new Subject<Film[]>();
   constructor() { }
+
+  emitFilms(){
+    this.filmsSubject.next(this.films);
+  }
+
+  saveFilms(){
+    firebase.database().ref('/films').set(this.films);
+  }
+
+  getFilms(){
+    firebase.database().ref('/films')
+      .on('value', (data) =>{
+          this.films = data.val() ? data.val() : [];
+          this.emitFilms();
+      });
+  }
+
+  getSingleFilm(id:number){
+    return new Promise(
+      (resolve, reject) =>{
+        firebase.database().ref('/films/'+id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+      }
+    )
+  }
+  createNewFilm(newFilm: Film){
+    this.films.push(newFilm);
+    this.emitFilms();
+  }
+
+  removeFilm(film: Film){
+    const filmIndexToRemove = this.films.findIndex(
+      (filmEl) =>{
+        if(filmEl === film){
+          return true;
+        }
+      }
+    );
+    this.films.splice(filmIndexToRemove, 1);
+    this.saveFilms();
+    this.emitFilms();
+  }
 }
