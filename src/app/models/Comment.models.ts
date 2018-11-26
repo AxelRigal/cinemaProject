7e9bc43b96e22fb36bf0134f798cacc27294ce77
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2018-11-26 16:53:12 +0100

    models

---
+++ src/app/models/Comment.models.ts
@@ -0,0 +1,11 @@
+export class Comment{
+
+
+    constructor(
+        public content: string,
+        public note: number,
+        public idFilm : number // sans doute à modifier check 
+    ){
+        
+    }
+}
